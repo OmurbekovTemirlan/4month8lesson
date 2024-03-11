c65d68040eef64548709ce9a04694f118c0b4f8a
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-03-11 17:44:06 +0600

    commit2

--- 4month8lesson/Protocols/MyProcols.swift
+++ 4month8lesson/Protocols/MyProcols.swift
@@ -5,4 +5,18 @@
 //  Created by Apple on 11.3.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol MyProcols {
+    func changeScreens(vc: UIViewController)
+}
+extension MyProcols {
+    func changeScreens(vc: UIViewController){
+        if let navigationController = vc.navigationController {
+            navigationController.pushViewController(vc, animated: true)
+        } else {
+            fatalError("The view controller must be embedded in a navigation controller.")
+        }
+    }
+}
+
--- 4month8lesson/Views/ViewController.swift
+++ 4month8lesson/Views/ViewController.swift
@@ -7,14 +7,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MyProcols {
 
+    
+    private let continueBtn = MakerView.myMake.makeButton(title: "Continue",backgroundColorBT: .blue, cornerRadiusBt: 15, target: self, action: #selector(continueBtnTapped) )
+    
+//    private let continueBtn: UIButton = {
+//        let btn = UIButton()
+//        btn.setTitle("Continue", for: .normal)
+//        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.layer.cornerRadius = 15
+//        btn.backgroundColor = .blue
+//        btn.addTarget(self, action: #selector(continueBtnTapped), for: .touchUpInside)
+//        return btn
+//    }()
+    
+    private let nextBtn = MakerView.myMake.makeButton(title: "Continue",backgroundColorBT: .blue, cornerRadiusBt: 15, target: self, action: #selector(nextBtnTapped) )
+    
+//    private let nextBtn: UIButton = {
+//        let btn = UIButton()
+//        btn.setTitle("Next", for: .normal)
+//        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.layer.cornerRadius = 15
+//        btn.backgroundColor = .brown
+//        btn.addTarget(self, action: #selector(nextBtnTapped), for: .touchUpInside)
+//
+//        return btn
+//    }()
+    
+    
+    
+    private let downloadBtn = MakerView.myMake.makeButton(title: "Continue",backgroundColorBT: .blue, cornerRadiusBt: 15, target: self, action: #selector(downloadBtnTapped) )
+    
+    
+    
+//    private let downloadBtn: UIButton = {
+//        let btn = UIButton()
+//        btn.setTitle("Dowload", for: .normal)
+//        btn.translatesAutoresizingMaskIntoConstraints = false
+//        btn.layer.cornerRadius = 15
+//        btn.addTarget(self, action: #selector(downloadBtnTapped), for: .touchUpInside)
+//
+//        btn.backgroundColor = .red
+//        return btn
+//    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        view.backgroundColor = .systemCyan
+        setupUI()
     }
 
+    private func setupUI(){
+        view.addSubview(continueBtn)
+        view.addSubview(nextBtn)
+        view.addSubview(downloadBtn)
+        
+        NSLayoutConstraint.activate([
+            continueBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            continueBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            continueBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            continueBtn.heightAnchor.constraint(equalToConstant: 50),
+            
+            nextBtn.topAnchor.constraint(equalTo: continueBtn.bottomAnchor, constant: 30),
+            nextBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            nextBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            nextBtn.heightAnchor.constraint(equalToConstant: 50),
+            
+            downloadBtn.topAnchor.constraint(equalTo: nextBtn.bottomAnchor, constant: 30),
+            downloadBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            downloadBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            downloadBtn.heightAnchor.constraint(equalToConstant: 50),
+        
+        ])
 
+    }
+    @objc func continueBtnTapped(_ sender: UIButton){
+       let vc = FirstViewController()
+        navigationController?.pushViewController(vc, animated: true)
+       
+    }
+    @objc func nextBtnTapped(){
+        let vc = SecondViewController()
+         navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    @objc func downloadBtnTapped(){
+        let vc = ThirdViewController()
+         navigationController?.pushViewController(vc, animated: true)
+        
+    }
 }
 
